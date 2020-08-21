aebe80f841ca8d84bac2f8032eb4116781e8260c
BurningAlfalfa <acmonahan@gmail.com>
2020-08-20 20:32:09 -0500

    foramat stock info

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,404 @@ import {
 } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import "./App.css";
+
 function StockPage() {
+  const [stockPriceData, setStockPriceData] = useState({});
+  const [yesterdayPriceData, setYesterdayPriceData] = useState({});
+  const [stockInfoData, setStockInfoData] = useState({});
   const history = useHistory();
   let { stockTick } = useParams();
+  console.log(stockTick);
+  useEffect(() => {
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockTick}&interval=5min&apikey=5NUW7CKWG93NYG2M`
+    )
+      .then((res) => res.json())
+      .then((res) => setStockPriceData(res));
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockTick}&apikey=5NUW7CKWG93NYG2M`
+    )
+      .then((res) => res.json())
+      .then((res) => setYesterdayPriceData(res));
+    fetch(
+      `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stockTick}&apikey=5NUW7CKWG93NYG2M`
+    )
+      .then((res) => res.json())
+      .then((res) => setStockInfoData(res));
+    // setInterval(
+    //   () =>
+    //     fetch(
+    //       `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockTick}&interval=5min&apikey=5NUW7CKWG93NYG2`
+    //     ),
+    //   1000 * 60 * 5
+    // );
+  }, []);
+  const stockDescription = stockInfoData["Description"];
+  const companyName = stockInfoData["Name"];
+  const marketCap = stockInfoData["MarketCapitalization"];
+  const employess = stockInfoData["FullTimeEmployees"];
+  const shares = stockInfoData["SharesFloat"];
+  const high52 = stockInfoData["52WeekHigh"];
+  const low52 = stockInfoData["52WeekLow"];
+  const dividend = stockInfoData["DividendDate"];
+  const exDiv = stockInfoData["ExDividendDate"];
+  const priceToEaring = stockInfoData["PERatio"];
+  const movingAverage50 = stockInfoData["50DayMovingAverage"];
+  const movingAverage200 = stockInfoData["200DayMovingAverage"];
+  const beta = stockInfoData["Beta"];
+  const divYield = stockInfoData["DividendYield"];
 
+  const yesterdayPrice = yesterdayPriceData["Time Series (Daily)"]
+    ? Number(
+        Object.values(yesterdayPriceData["Time Series (Daily)"])[1]["4. close"]
+      ).toFixed(2)
+    : "error";
+  const stockPrice = stockPriceData["Time Series (5min)"]
+    ? Number(
+        Object.values(stockPriceData["Time Series (5min)"])[0]["1. open"]
+      ).toFixed(2)
+    : "error";
+
+  const stockPriceDate = stockPriceData["Time Series (5min)"]
+    ? Object.keys(stockPriceData["Time Series (5min)"])[0]
+    : "error";
+  const isPositive = stockPrice >= yesterdayPrice;
+  console.log(stockPriceData);
   return (
-    <div>
-      <div></div>
+    <div className="body-wrapper">
+      <div>
+        <div className="box">
+          <h1>Stock Price</h1>
+          <div>{stockPriceDate}</div>
+          <div className="stock-ticker">{stockTick}</div>
+          <div
+            style={{ color: isPositive ? "green" : "red" }}
+            className="stock-price"
+          >
+            {stockPrice}
+          </div>
+        </div>
+        <div className="box">
+          <p>Company Description</p>
+          {stockDescription}
+        </div>
+        <div class="columns is-mobile is-multiline is-centered">
+          <div className="column is-12-mobile is-10-tablet is-8-fullhd">
+            <div class="columns box is-paddingless is-marginless">
+              <div class="column">
+                <h1> Stock Info</h1>
+
+                <div class="columns is multiline is-marginless is-paddingless">
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Company Name</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatValue">
+                        <p>{companyName}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Market Cap</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{marketCap}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Fulltime Employess</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{employess}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Float</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{shares}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Average Vol (30)</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Avg Vol (10) </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> Next Earnings </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> TTMEPS </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> TTM Div Rate </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> Div Yield </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{divYield}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="column is-12-mobile is-6-tablet is-6-desktop is-4-widescreen is-4-fullhd is-paddingless is-size-7 dbStatWrapper tooltip is-tooltip-info is-tooltip-multiline">
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> Next Div </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{dividend}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p> Ex-Div </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{exDiv}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>PE</p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{priceToEaring}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>Beta </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{beta}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="columns is-mobile is-marginless is-paddingless">
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>52W High </p>
+                      </div>
+                      <div class="column is-paddingless is-half dbStatLabel">
+                        <p>{high52}</p>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div>
+                    <div>
+                      <div>
+                        <p>52W Low</p>
+                      </div>
+                      <div>
+                        <p> {low52}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>52W Change </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>50MA</p>
+                      </div>
+                      <div>
+                        <p> {movingAverage50}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>200DMA </p>
+                      </div>
+                      <div>
+                        <p>{movingAverage200}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (5d) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p> Change (30d)</p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (1m) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (3m)</p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (6m)</p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p> Change (1y) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p> Change (2y) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (5y) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (YTD)</p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Change (Max) </p>
+                      </div>
+                      <div>
+                        <p>-</p>
+                      </div>
+                    </div>
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -78,7 +469,7 @@ function App() {
     <>
       <Router>
         <Switch>
-          <Route path={"/"}>
+          <Route exact path={"/"}>
             <HomePage />
           </Route>
           <Route path={`/:stockTick`}>
