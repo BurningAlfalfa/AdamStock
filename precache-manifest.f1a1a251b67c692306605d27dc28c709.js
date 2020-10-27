c81661d9e402fba41f32997e75950744251c1e86
BurningAlfalfa <acmonahan@gmail.com>
2020-10-26 19:35:28 -0500

    Updates

---
+++ precache-manifest.f1a1a251b67c692306605d27dc28c709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8462d9cdecb9c79a7b66ef61dac95d",
+    "url": "/AdamStock/index.html"
+  },
+  {
+    "revision": "b1fee05aaee05b7fa41f",
+    "url": "/AdamStock/static/css/main.df3d0527.chunk.css"
+  },
+  {
+    "revision": "51c6cf8740e51a64ee73",
+    "url": "/AdamStock/static/js/2.459ac960.chunk.js"
+  },
+  {
+    "revision": "f97c7fb49912a805ec95d3d3783f69dd",
+    "url": "/AdamStock/static/js/2.459ac960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fee05aaee05b7fa41f",
+    "url": "/AdamStock/static/js/main.8423c772.chunk.js"
+  },
+  {
+    "revision": "3e0c60d4ed4dcadafce7",
+    "url": "/AdamStock/static/js/runtime-main.e07e917b.js"
+  }
+]);
